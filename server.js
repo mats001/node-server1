a1494345774750a7c2e00156bb398e5349e5f3fb
mats001 <anilm@sculpturehospitality,asia>
2018-07-30 18:40:55 +0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app=express();
+
+hbs.registerPartials(__dirname + "/views/partials")
+hbs.registerHelper('curYear',()=>{
+  return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+})
+app.set('view-engine','hbs');
+app.use((req,res,next)=>{
+  let now = new Date().toString();
+  let msg =`${now} : ${req.method} ${req.url}` + '\n'
+  fs.appendFile('log.txt',msg,(err)=>{
+    if (err){
+      console.log(err) ;
+    }
+  })
+  next();
+});
+// app.use((req,res,next)=>{
+//   res.render('maint.hbs')
+// });
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    pageName:'Amayalandias Page',
+    messageOfTheDay:'Welcome to my site'
+  });
+});
+app.get("/about",(req,res)=>{
+  res.render('about.hbs',{
+    pageTitel:'About Page',
+    pageName:'About Page',
+  });
+});
+app.get("/bad",(req,res)=>{
+  res.send("<h1 align='center'>Work in progress, please visit us later</h1>");
+});
+app.listen(3000,()=>{
+  console.log('App listening on port 3000');
+});
